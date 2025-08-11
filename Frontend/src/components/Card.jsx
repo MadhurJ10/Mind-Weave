dd852dff8bbe7c34ed8d6ce903bee02ca48674b4
Madhur Bhawsar <madhurbhawsar7@gmail.com>
2025-08-11 12:55:01 +0530

    card added

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-const Card = () => {
+const Card = ({ title , heading}) => {
   return (
-    <div>
-      
+    <div className="max-w-sm w-full mx-auto p-8  rounded-xl border border-[rgba(255,255,255,0.10)] dark:bg-[rgba(40,40,40,0.70)] bg-gray-100 shadow-[2px_4px_16px_0px_rgba(248,248,248,0.06)_inset] group ">
+      <div className=' text-white5'>
+        <h1>dfklndsfndsjlfn</h1>
+      </div>
+      <p className="text-lg font-semibold text-gray-800 dark:text-white py-2">
+        {title}
+      </p>
+      <p className="text-sm font-normal text-neutral-600 dark:text-neutral-400 max-w-sm">
+        {heading}
+      </p>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
--- Frontend/src/components/Pattern.jsx
+++ Frontend/src/components/Pattern.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import Card from './Card';
 
 const Pattern = () => {
   return (
     <StyledWrapper>
       <div className="container">
         <h1 className='text-white'>snjdnaj</h1>
-        </div>     
+        <div className='flex flex-col sm:flex-row'>
+          <Card title={'AI Map Creation'} heading={'Generate structured, connected maps instantly from just a single topic.'} />
+          <Card title={'Ask AI'} heading={'Get instant explanations or discover hidden connections between nodes.'} />
+          <Card title={'Export & Share'} heading={'Download as PNG or PDF, or import into other tools for seamless use.'} />
+          
+
+        </div>
+      </div>
     </StyledWrapper>
   );
 };
